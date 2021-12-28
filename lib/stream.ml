3ce2022aa3dbc3d6f198e5a00534e0b9680c590c
Jordan Scales <scalesjordan@gmail.com>
2021-12-28 14:04:52 -0500

    map and generate

--- lib/stream.ml
+++ lib/stream.ml
@@ -32,3 +32,49 @@ let rec drop n s = match n with
   | n -> drop (n - 1) (tail s)
 
 let%test _ = [4;5;6;7] = (nums |> drop 4 |> take 4)
+
+let rec map f s =
+  Stream (f (head s), fun _ -> map f (tail s))
+
+let%test _ = [0;2;4;6;8] = (map (( * ) 2) nums |> take 5)
+
+let fizzbuzz =
+  let count n =
+    if n mod 15 = 0
+    then "FizzBuzz"
+    else if n mod 3 = 0
+    then "Fizz"
+    else if n mod 5 = 0
+    then "Buzz"
+    else string_of_int n
+  in let nats = map ((+) 1) nums
+  in map count nats
+
+let%test _ = ["1";"2";"Fizz";"4";"Buzz";"Fizz";"7"] = take 7 fizzbuzz
+
+let rec generate f init =
+  Stream (init, fun _ -> generate f (f init))
+
+let%test _ = take 10 ones = take 10 (generate (fun x -> x) 1)
+let%test _ = take 10 nums = take 10 (generate ((+) 1) 0)
+
+let fib =
+  let pairs =
+    generate (fun (a, b) -> (b, a + b)) (0, 1)
+  in map (fun (a, _) -> a) pairs
+
+let%test _ = [0;1;1;2;3;5;8;13;21] = take 9 fib
+
+let collatz n =
+  generate 
+    (
+      fun n -> 
+        if n mod 2 = 0 
+        then n / 2
+        else 3 * n + 1
+    )
+    n
+
+let%test _ =
+  [7;22;11;34;17;52;26;13;40;20;10;5;16;8;4;2;1]
+  = take 17 (collatz 7)
